2ef4296bd83ace86ef650f2758c68b2b8ef07b47
Walinns <walisdemo1@gmail.com>
2018-04-11 09:32:31 +0530

    hbs used

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // router.use(cors());
 
 var corsOptions = {
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
